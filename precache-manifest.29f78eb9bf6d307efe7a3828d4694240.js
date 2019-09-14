bb29f661c25fd5bd9653ac219f28ac39805377df
alexDemche <alexeyydemchenko@gmail.com>
2019-09-14 12:34:53 +0300

    Updates

---
+++ precache-manifest.29f78eb9bf6d307efe7a3828d4694240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90caec95c3e46ef83991272c5b3449b8",
+    "url": "/react_carousel/index.html"
+  },
+  {
+    "revision": "84a2dd879e7c7e7ad8b8",
+    "url": "/react_carousel/static/css/main.0a6df596.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_carousel/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "84a2dd879e7c7e7ad8b8",
+    "url": "/react_carousel/static/js/main.9f50c47d.chunk.js"
+  },
+  {
+    "revision": "a62ff4406f9253df9292",
+    "url": "/react_carousel/static/js/runtime~main.30902e3a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_carousel/precache-manifest.50801b1752512efc4ec66a2e23e808f4.js"
+  "/react_carousel/precache-manifest.29f78eb9bf6d307efe7a3828d4694240.js"
 );
 
 self.addEventListener('message', (event) => {
